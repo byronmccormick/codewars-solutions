a7c8e5730ae2a010e14a608c83b15d1cfefb6c4b
byron mccormick <byronjamesmccormick@gmail.com>
2022-08-04 09:05:52 -0600

    Daily Challenge

---
+++ 2022-08-04/codewars.js
@@ -0,0 +1,13 @@
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+//
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+function reverseWords(str) {
+  let arr = str.split(' ')
+  for(let i = 0; i < arr.length; i++){
+   arr[i] = arr[i].split('').reverse().join('')
+  }
+  return arr.join(' ')
+}
