e5d3474be82ea401775baa801b244f6f3a986b1e
byron mccormick <byronjamesmccormick@gmail.com>
2022-08-18 08:51:42 -0600

    Daily Challenge

---
+++ 2022-08-18/codewars.js
@@ -0,0 +1,51 @@
+// Welcome.
+//
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+//
+// If anything in the text isn't a letter, ignore it and don't return it.
+//
+// "a" = 1, "b" = 2, etc.
+//
+// Example
+// alphabetPosition("The sunset sets at twelve o' clock.")
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+
+
+function alphabetPosition(text) {
+  let str = ""
+  text = text.toUpperCase();
+  let ob = {
+    "A": 1,
+    "B": 2,
+    "C": 3,
+    "D": 4,
+    "E": 5,
+    "F": 6,
+    "G": 7,
+    "H": 8,
+    "I": 9,
+    "J": 10,
+    "K": 11,
+    "L": 12,
+    "M": 13,
+    "N": 14,
+    "O": 15,
+    "P": 16,
+    "Q": 17,
+    "R": 18,
+    "S": 19,
+    "T": 20,
+    "U": 21,
+    "V": 22,
+    "W": 23,
+    "X": 24,
+    "Y": 25,
+    "Z": 26
+  }
+  for(let i = 0; i < text.length; i++){
+    if(text[i].match(/[a-z]/i)){
+      str += " " + ob[text[i]]
+    }
+  }
+  return str.substr(1)
