943f53dd2146122b063d96e1a9455e23165e4e39
Byron McCormick <byronjamesmccormick@gmail.com>
2023-01-02 05:33:31 +0000

    Daily Challenge

---
+++ 2023-01-01/codewars.js
@@ -0,0 +1,14 @@
+
+
+function switcheroo(x){
+  x=x.split('')
+  for(let i = 0; i < x.length; i++){
+    if(x[i]=='a'){
+      x[i] = 'b'
+    }else if(x[i]=='b'){
+      x[i] = 'a'
+    }
+    
+  }
+  return x.join('')
+}
