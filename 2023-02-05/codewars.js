70ccfcec8199cfea362f895c77bd10799595828d
Byron McCormick <byronjamesmccormick@gmail.com>
2023-02-05 11:17:21 -0700

    Daily Challenege

---
+++ 2023-02-05/codewars.js
@@ -0,0 +1,25 @@
+// We want to know the index of the vowels in a given word, for example, there are two vowels in the word super (the second and fourth letters).
+
+// So given a string "super", we should return a list of [2, 4].
+
+// Some examples:
+// Mmmm  => []
+// Super => [2,4]
+// Apple => [1,5]
+// YoMama -> [1,2,4,6]
+// NOTES
+// Vowels in this context refers to: a e i o u y (including upper case)
+// This is indexed from [1..n] (not zero indexed!)
+
+function vowelIndices(word){
+    let arr = []
+    let i = 0
+   let vowels = ['a','e','i','o','u','y','A','E','I','O','U','Y']
+   for(let char of word){
+     if(vowels.includes(char)){
+       arr.push(word.indexOf(char,i)+1)
+     }
+     i++
+   }
+   return arr 
+  }
