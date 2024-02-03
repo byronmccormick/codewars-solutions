04a6e8181d041fb3bb3a2d7ee190c2f9e5ba5306
Byron McCormick <byronjamesmccormick@gmail.com>
2024-02-03 12:32:18 -0700

    Daily challenge

---
+++ 2024-02-04/codewars.js
@@ -0,0 +1,22 @@
+// Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
+
+// For example:
+
+// A -> 1
+// B -> 2
+// C -> 3
+// ...
+// Z -> 26
+// AA -> 27
+// AB -> 28 
+
+var titleToNumber = function(columnTitle) {
+    let result = 0;
+
+    for (let i = 0; i < columnTitle.length; i++) {
+        const charCode = columnTitle.charCodeAt(i) - 'A'.charCodeAt(0) + 1;
+        result = result * 26 + charCode;
+    }
+
+    return result;
+};
